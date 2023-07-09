b5da30fcdb3ebe81d00a4b29f96d990f0b506f5a
ryumoon88 <naufalhady08@yahoo.com>
2023-07-09 15:14:06 +0700

    Add user and role resources/page

--- database/seeders/ShieldSeeder.php
+++ database/seeders/ShieldSeeder.php
@@ -15,8 +15,8 @@ class ShieldSeeder extends Seeder
     {
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        $rolesWithPermissions = '[{"name":"super_admin","guard_name":"web","permissions":["view_user","view_any_user","create_user","update_user","restore_user","restore_any_user","replicate_user","reorder_user","delete_user","delete_any_user","force_delete_user","force_delete_any_user","view_role","view_any_role","create_role","update_role","delete_role","delete_any_role"]},{"name":"filament_user","guard_name":"web","permissions":[]}]';
-        $directPermissions = '[{"name":"view_shield::role","guard_name":"web"},{"name":"view_any_shield::role","guard_name":"web"},{"name":"create_shield::role","guard_name":"web"},{"name":"update_shield::role","guard_name":"web"},{"name":"delete_shield::role","guard_name":"web"},{"name":"delete_any_shield::role","guard_name":"web"}]';
+        $rolesWithPermissions = '[{"name":"super_admin","guard_name":"web","permissions":["view_user","view_any_user","create_user","update_user","restore_user","restore_any_user","replicate_user","reorder_user","delete_user","delete_any_user","force_delete_user","force_delete_any_user","view_shield::role","view_any_shield::role","create_shield::role","update_shield::role","delete_shield::role","delete_any_shield::role"]},{"name":"filament_user","guard_name":"web","permissions":[]}]';
+        $directPermissions = '{"12":{"name":"view_role","guard_name":"web"},"13":{"name":"view_any_role","guard_name":"web"},"14":{"name":"create_role","guard_name":"web"},"15":{"name":"update_role","guard_name":"web"},"16":{"name":"delete_role","guard_name":"web"},"17":{"name":"delete_any_role","guard_name":"web"}}';
 
         static::makeRolesWithPermissions($rolesWithPermissions);
         static::makeDirectPermissions($directPermissions);
