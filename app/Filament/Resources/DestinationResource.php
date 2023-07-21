e3195e45f5bae9f45092c830675e3ed0e3a80094
ryumoon88 <naufalhady08@yahoo.com>
2023-07-21 17:48:14 +0700

    Add destination resource page

---
+++ app/Filament/Resources/DestinationResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DestinationResource\Pages;
+use App\Filament\Resources\DestinationResource\RelationManagers;
+use App\Models\Destination;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DestinationResource extends Resource
+{
+    protected static ?string $model = Destination::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDestinations::route('/'),
+            'create' => Pages\CreateDestination::route('/create'),
+            'view' => Pages\ViewDestination::route('/{record}'),
+            'edit' => Pages\EditDestination::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ app/Filament/Resources/DestinationResource/Pages/ViewDestination.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\DestinationResource\Pages;
+
+use App\Filament\Resources\DestinationResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewDestination extends ViewRecord
+{
+    protected static string $resource = DestinationResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+        ];
+    }
+}
