cbc91e47b1a38783ba2df0dedf3df1d698fea4dd
ryumoon88 <naufalhady08@yahoo.com>
2023-07-23 20:50:59 +0700

    Add destination service model and database.

---
+++ database/migrations/2023_07_23_134503_create_destination_service_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destination_service_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id');
+            $table->foreignId('user_id');
+            $table->enum('status', ['Sent', 'Received', 'Accepted', 'Rejected'])->default('Sent');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destination_service_orders');
+    }
+};
